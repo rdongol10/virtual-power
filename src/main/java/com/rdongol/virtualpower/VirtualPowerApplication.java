277545f0dfde01621dad56dcb7d4a9ab7ed98cff
Rabin Dongol <rawbean08@gmail.com>
2022-08-18 08:35:25 +0545

    initial commit

---
+++ src/main/java/com/rdongol/virtualpower/VirtualPowerApplication.java
@@ -0,0 +1,13 @@
+package com.rdongol.virtualpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPowerApplication.class, args);
+	}
+
+}
