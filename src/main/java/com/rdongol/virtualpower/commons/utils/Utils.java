624635426fad258b039b52d39f13b67b62d9fcc8
Rabin Dongol <rawbean08@gmail.com>
2022-08-18 15:55:50 +0545

    removed unused method

--- src/main/java/com/rdongol/virtualpower/commons/utils/Utils.java
+++ src/main/java/com/rdongol/virtualpower/commons/utils/Utils.java
@@ -3,9 +3,6 @@ package com.rdongol.virtualpower.commons.utils;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Utils {
 
     public static <T> T mapObject(Object fromValue, Class<T> toValueType) {
@@ -14,9 +11,4 @@ public class Utils {
         return mapper.convertValue(fromValue, toValueType);
     }
 
-    public static <T> List<T> mapObjectList(List<Object> fromValues , Class<T> toValueType){
-        return fromValues.stream().map(
-                        fromValue -> Utils.mapObject(fromValue, toValueType))
-                .collect(Collectors.toList());
-    }
 }
